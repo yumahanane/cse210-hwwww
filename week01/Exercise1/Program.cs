bd29ef7d91fb4314949d3eb8dfed94c1bc16d670
yumahanane <yuma.hanane1@gmail.com>
2025-05-07 17:31:24 +0200

    Variables names changed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,11 +7,11 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
 
         Console.Write("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
