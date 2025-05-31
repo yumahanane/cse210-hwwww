946ac06c72a8d4d9387ad6207c6199192f1f4bed
yumahanane <yuma.hanane1@gmail.com>
2025-05-31 23:03:27 +0200

    fraction added

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,15 @@
+/* 
+Class: Fraction
+Attributes:
+* _top: int
+* _bottom: int
+
+Constructors:
+* Fraction()
+* Fraction(wholeNumber: int)
+* Fraction(top: int, bottom: int)
+
+Behaviors:
+* GetTop()
+* SetTop()
+*/
