74707bbe936b232e45c6782d2d127124dd97b78c
yumahanane <yuma.hanane1@gmail.com>
2025-06-03 17:01:32 +0200

    Fraction completed

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,3 +1,4 @@
+using System;
 /* 
 Class: Fraction
 Attributes:
@@ -9,7 +10,66 @@ Constructors:
 * Fraction(wholeNumber: int)
 * Fraction(top: int, bottom: int)
 
-Behaviors:
+Getters and Setters:
 * GetTop()
-* SetTop()
+* SetTop(top: int)
+* GetBottom()
+* SetBottom(bottom: int)
+
+Behaviors:
+* GetFractionString(): string
+* GetDecimalValue(): double
 */
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        //string Text = $"{_top}/{_bottom}";
+        //return Text; 
+        // or it can also be written
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
+
+
+
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction f1 = new Fraction();
+        //f1.SetTop(1);
+        //Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        //f2.SetTop(5);
+        //Console.WriteLine(f2.GetTop());
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        //f3.SetTop(3);
+        //f3.SetBottom(4);
+        //Console.WriteLine(f3.GetTop());
+        //Console.WriteLine(f3.GetBottom());
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        // or 
+        /*
+        Fraction f3 = new Fraction();
+        //f3.SetTop(3);
+        //f3.SetBottom(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+    }
+        */
     }
 }
